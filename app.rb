d161444f84d6bcd0682113ee33f40b9121660d68
Jared Harbison <jared.harbison@gmail.com>
2019-02-24 23:10:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ class App < Sinatra::Base
     "Hello, World!"
   end
 
-end
+  get '/name'
+    "My name is Rage Briarcliff"
+  end
+
+  get '/hometown'
+    "I'm from Paris"
+  end
+
+  get '/favorite-song'
+    "And my favorite song is Friday"
+  end
+
+end 
